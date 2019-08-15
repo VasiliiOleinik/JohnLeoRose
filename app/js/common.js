bfc4bb3cc29832f3e0614049708096771d8a2d83
Vasilii <goooneoooogle@gmail.com>
2019-08-16 01:03:46 +0300

    choose and mens

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,26 @@
 $(function(){
   
-  // Убрать верхний информационный блок
+  // Remove top info block
   $('#close-header-info').on('click', function(){
     $('#header-info-block').remove();
+  });
+
+  // Main slider
+  $(".mens-told__slider.owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
   });
 });
